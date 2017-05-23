21023482ca82e12b9826d9cf138240be680e94ab
bjfreedman <brad_freedman@hotmail.com>
2017-05-23 20:38:04 +1000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -241,8 +241,8 @@ class FacebookBot {
         if (event.postback) {
                 
             if (event.postback.referral.ref) { 
-                //return event.postback.referral.ref;
-                 return event.postback.payload;
+                 return event.postback.referral.ref;
+                 event.postback.referral.ref = [];
             }
             
             else {
