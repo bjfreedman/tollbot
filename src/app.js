7d75250957f4676befafbd4495c68aadf5d2e7e6
bjfreedman <brad_freedman@hotmail.com>
2017-05-24 23:20:10 +1000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -240,7 +240,9 @@ class FacebookBot {
 
         if (event.postback) {
                 if (event.postback.referral.ref) {
+                    console.log(event.postback.payload);
                     console.log(event.postback.referral.ref);
+                  
                     return event.postback.referral.ref;
                     
                     //console.log(event.postback.payload);
